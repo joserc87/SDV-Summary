a04f89b13cb00fc06485d97e7411c0acddfddedf
Ashley <Ashley.john.eldridge@gmail.com>
2016-03-09 00:32:14 +0100

    further work on the farm renderer

--- farmInfo.py
+++ farmInfo.py
@@ -14,16 +14,42 @@ def getFarmInfo(saveFileLocation):
 	root = xml.etree.ElementTree.parse(saveFileLocation).getroot()
 
 	locations = root.find('locations').findall("GameLocation")
+	# things = []
 	s = []
 	for item in locations[1].find('objects').iter("item"):
 		name = item.find('value').find('Object').find('Name').text
 		x = int(item.find('value').find('Object').find('tileLocation').find('X').text)
 		y = int(item.find('value').find('Object').find('tileLocation').find('Y').text)
+		# if name not in things:
+		# 	things.append(name)
 		s.append((name, x, y))
+
+	# print(things)
 
 	farm['objects'] = s
 
-	return s
+	s = []
+
+	for item in locations[1].find('terrainFeatures').iter('item'):
+		name = item.find('value').find('TerrainFeature').get(ns+'type')
+		x = int(item.find('key').find('Vector2').find('X').text)
+		y = int(item.find('key').find('Vector2').find('Y').text)
+		s.append((name, x, y))
+
+	farm['terrainFeatures'] = s
+
+	s = []
+
+	for item in locations[1].find('resourceClumps').iter('ResourceClump'):
+		x = int(item.find('tile').find('X').text)
+		y = int(item.find('tile').find('Y').text)
+		w = int(item.find('width').text)
+		h = int(item.find('height').text)
+		s.append((x, y, w, h))
+
+	farm['resourceClumps'] = s
+
+	return farm
 
 def colourBox(x, y, colour, pixels, scale = 8):
 	for i in range(scale):
@@ -31,30 +57,49 @@ def colourBox(x, y, colour, pixels, scale = 8):
 			pixels[x*scale+ i, y*scale + j] = colour
 	return pixels
 
-def makeImage():
-	newImage = Image.new('RGB', (80*8, 70*8))
-	pixels = newImage.load()
-	for i in range(80*8):
-		for j in range(70*8):
-			pixels[i,j] = (255, 224, 102)
+def generateImage():
+	image = Image.open(".//data//img//base.png")
+	pixels = image.load()
 
 	farm = getFarmInfo('./save/Sketchy_116441313')
 
-	for tile in farm:
-		if tile[0] == "Weeds":
+	for tile in farm['terrainFeatures']:
+		name = tile[0]
+		if name == "Tree":
+			colourBox(tile[1], tile[2], (0,175,0), pixels)
+		elif name == "Grass":
+			colourBox(tile[1], tile[2], (0,125,0), pixels)
+		elif name == "HoeDirt":
+			colourBox(tile[1], tile[2], (196,196,38), pixels)
+		elif name == "Flooring":
+			colourBox(tile[1], tile[2], (50,50,50), pixels)
+		else:
+			colourBox(tile[1], tile[2], (0,0,0), pixels)
+
+	for tile in farm['objects']:
+		name= tile[0]
+		if name == "Weeds":
 			colourBox(tile[1], tile[2], (0,255,0), pixels)
-		elif tile[0] == "Stone":
+		elif name == "Stone":
 			colourBox(tile[1], tile[2], (125,125,125), pixels)
-		elif tile[0] == "Twig":
+		elif name == "Twig":
 			colourBox(tile[1], tile[2], (153,102,51), pixels)
-		elif tile[0] != "Chest":
+		elif 'Fence' in name:
+			colourBox(tile[1], tile[2], (200,200,200), pixels)
+		elif name != "Chest":
 			colourBox(tile[1], tile[2], (255,0,0), pixels)
 
-	newImage.save("test.png")
+	for tile in farm['resourceClumps']:
+		for i in range(tile[2]):
+			for j in range(tile[3]):
+				colourBox(tile[0] + i, tile[1] + j, (75,75,75), pixels)
+
+
+	image.save("test.png")
 
 def main():
 	getFarmInfo('./save/Sketchy_116441313')
-	makeImage()
+	generateImage()
 
 if __name__ == '__main__':
 	main()
