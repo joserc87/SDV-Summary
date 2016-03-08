f3b4e84ad20671ec592b4ec011abe073d0826e98
Ashley <Ashley.john.eldridge@gmail.com>
2016-03-08 22:06:16 +0100

    Very rough farm image renderer implemented

--- farmInfo.py
+++ farmInfo.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree
+from PIL import Image 
 
 # This is a test method for returning the position location and the name of objects
 # located on the players farm.
@@ -16,16 +17,44 @@ def getFarmInfo(saveFileLocation):
 	s = []
 	for item in locations[1].find('objects').iter("item"):
 		name = item.find('value').find('Object').find('Name').text
-		x = item.find('value').find('Object').find('tileLocation').find('X').text
-		y = item.find('value').find('Object').find('tileLocation').find('Y').text
+		x = int(item.find('value').find('Object').find('tileLocation').find('X').text)
+		y = int(item.find('value').find('Object').find('tileLocation').find('Y').text)
 		s.append((name, x, y))
 
 	farm['objects'] = s
 
-	return farm
+	return s
+
+def colourBox(x, y, colour, pixels, scale = 8):
+	for i in range(scale):
+		for j in range(scale):
+			pixels[x*scale+ i, y*scale + j] = colour
+	return pixels
+
+def makeImage():
+	newImage = Image.new('RGB', (80*8, 70*8))
+	pixels = newImage.load()
+	for i in range(80*8):
+		for j in range(70*8):
+			pixels[i,j] = (255, 224, 102)
+
+	farm = getFarmInfo('./save/Sketchy_116441313')
+
+	for tile in farm:
+		if tile[0] == "Weeds":
+			colourBox(tile[1], tile[2], (0,255,0), pixels)
+		elif tile[0] == "Stone":
+			colourBox(tile[1], tile[2], (125,125,125), pixels)
+		elif tile[0] == "Twig":
+			colourBox(tile[1], tile[2], (153,102,51), pixels)
+		elif tile[0] != "Chest":
+			colourBox(tile[1], tile[2], (255,0,0), pixels)
+
+	newImage.save("test.png")
 
 def main():
-	print(getFarmInfo('./save/Sketchy_116441313'))
+	getFarmInfo('./save/Sketchy_116441313')
+	makeImage()
 
 if __name__ == '__main__':
 	main()
