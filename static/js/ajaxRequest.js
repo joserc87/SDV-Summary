f8a945c88579f129cce81967605019844d6db48d
Ashley <Ashley.john.eldridge@gmail.com>
2016-03-14 23:02:29 +0100

    re-added missing space in farm name

--- static/js/ajaxRequest.js
+++ static/js/ajaxRequest.js
@@ -12,7 +12,7 @@ function ajaxRequest(){
 }
 
 function recentTemplate(data){
-	$('#recentFarms').append('<div class="col-md-4 col-sm-6 text-center previewbox"><a href="'+data[0]+'"><div class="previewimage"><img src="'+data[5]+'" class="img-responsive farmimgpreview"><img src="'+data[4]+'" class="headimg" ></div><div class="previewtext">'+data[1]+', '+data[2]+'Farm <br/>'+data[3]+'</div></a></div>');
+	$('#recentFarms').append('<div class="col-md-4 col-sm-6 text-center previewbox"><a href="'+data[0]+'"><div class="previewimage"><img src="'+data[5]+'" class="img-responsive farmimgpreview"><img src="'+data[4]+'" class="headimg" ></div><div class="previewtext">'+data[1]+', '+data[2]+' Farm <br/>'+data[3]+'</div></a></div>');
 }
 
 $(document).ready(function(){
