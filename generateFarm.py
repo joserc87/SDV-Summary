7cdf3dc1deb3872abf644e55a2f61d87080f3a6b
Ashley <Ashley.john.eldridge@gmail.com>
2016-03-21 20:27:08 +0100

    initial commit for farm generation

---
+++ generateFarm.py
@@ -0,0 +1,12 @@
+from PIL import Image
+from playerInfo import playerInfo
+from farmInfo import getFarmInfo
+
+def generateFarm(player, farm):
+	pass
+
+def main():
+	generateFarm()
+
+if __name__ == '__main__':
+	main()
