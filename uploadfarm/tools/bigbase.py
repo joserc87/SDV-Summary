db36e6a839bd4a282ab50b812c2e52244ad6e1f9
Ashley <Ashley.john.eldridge@gmail.com>
2016-05-21 18:19:37 +0200

    add tools folder for misc scripts

---
+++ uploadfarm/tools/bigbase.py
@@ -0,0 +1,39 @@
+#
+# converts base 10 to base 62 for purpose of small URL identifiers
+#
+import sys
+
+CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+if sys.version_info > (3,):
+	long = int
+
+def dec2big(num):
+	assert type(num) in [int,long]
+	if num == 0:
+		output = CHARS[0]
+	elif num > 0:
+		base = len(CHARS)
+		output = ''
+		while num > 0:
+			rem = int(num%base)
+			output = CHARS[rem] + output
+			num = (num-rem)/base
+	else:
+		raise IOError
+	return output
+
+def big2dec(num):
+	assert all([num[x] in CHARS for x in range(len(num))])
+	base = len(CHARS)
+	output = 0
+	multiplier = 1
+	for item in num[::-1]:
+		output += multiplier * CHARS.index(item)
+		multiplier *= base
+	return output
+
+
+if __name__ == "__main__":
+	print(dec2big(9999999999999999999999999999999999999999999999999999999))
+	print(big2dec('gUFRx61gTwUoLRUcYcoUr2SbzgUyBQz'))
---
+++ uploadfarm/tools/validator.py
@@ -0,0 +1,33 @@
+partners = [
+            'alex', 'elliott', 'harvey', 'sam', 'sebastian',
+            'abigail', 'haley', 'leah', 'maru', 'penny'
+            ]
+
+likeable_npcs = partners + [
+                                'caroline', 'clint', 'demetrius', 'dwarf',
+                                'emily', 'evelyn', 'george', 'gus', 'jas',
+                                'jodi', 'kent', 'krobus', 'lewis', 'linus',
+                                'marnie', 'pam', 'pierre', 'robin', 'sandy',
+                                'shane ', 'vincent', 'willy', 'wizard'
+                                ]
+
+npcs = likeable_npcs + [
+                            'bouncer', 'gil', 'governor', 'grandpa', 'gunther',
+                            'marlon', 'morris', 'mr. qi'
+                            ]
+
+professions = [
+                'Rancher', 'Tiller', 'Coopmaster', 'Shepherd', 'Artisan',
+                'Agriculturist', 'Fisher', 'Trapper', 'Angler', 'Pirate',
+                'Mariner', 'Luremaster', 'Forester', 'Gatherer', 'Lumberjack',
+                'Tapper', 'Botanist', 'Tracker', 'Miner', 'Geologist',
+                'Blacksmith', 'Prospector', 'Excavator', 'Gemologist',
+                'Fighter', 'Scout', 'Brute', 'Defender', 'Acrobat', 'Desperado'
+                ]
+
+valid = {
+    'marriageCandidates': partners,
+    'likeableNPCs': likeable_npcs,
+    'NPCs': npcs,
+    'professions': professions
+    }
