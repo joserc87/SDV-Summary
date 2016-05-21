630dfcef9859703a5c86c18165ac1db9d8596458
Ashley <Ashley.john.eldridge@gmail.com>
2016-05-21 18:18:20 +0200

    add a simple initialise script

--- initialise.py
+++ initialise.py
@@ -1,1 +1,14 @@
-from uploadfarm.tools import createdb, createadmin
+import sys
+from uploadfarm.tools.createdb import initialisedb, updatedb
+from uploadfarm.tools.createadmin import initialiseAdmin
+
+if __name__ == '__main__':
+    if sys.version_info >= (3, 0):
+        raw_input = input
+
+    if raw_input('Initialise admin tables? (y/n)') == 'y':
+            initialiseAdmin()
+    if raw_input('Initialise data tables? (y/n)') == 'y':
+            initialisedb()
+    if raw_input('Update tables? (y/n)') == 'y':
+            updatedb()
