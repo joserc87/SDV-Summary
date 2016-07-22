b8b4d0119de932be4caf16a14c22cefb83b4819b
Ashley <Ashley.john.eldridge@gmail.com>
2016-07-22 12:38:06 +0200

    add liniting and testing to manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-
+import sys
+import subprocess
 from flask_script import Manager
 from sdv import app
 from sdv.createdb import init_db
@@ -9,7 +10,25 @@ manager = Manager(app)
 
 @manager.command
 def createdb(drop_all=False):
+    """Initialise Database."""
     init_db(drop_all)
+
+
+@manager.command
+def test():
+    """Run Unit Tests."""
+    tests = subprocess.call(['python', '-c', 'import tests; tests.run()'])
+    sys.exit(tests)
+
+
+@manager.command
+def lint():
+    """Run Flake8 Linter."""
+    lint = subprocess.call(['python', '-m', 'flake8', '--ignore=E402', 'sdv/',
+                            'manage.py']) == 0
+    if lint:
+        print('OK')
+    sys.exit(lint)
 
 if __name__ == "__main__":
     manager.run()
