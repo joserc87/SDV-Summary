17e8950729564fa0ab67b3226a0d20522599709f
Sketchy502 <ashley.john.eldridge@gmail.com>
2016-10-02 23:19:39 +0100

    removed test file

--- render_farm.py
+++
@@ -1,8 +0,0 @@
-from sdv.imagegeneration.farm import generateFarm
-from sdv.farmInfo import getFarmInfo
-from sdv.playerInfo import playerInfo
-from sdv.savefile import savefile
-
-save = savefile('emi')
-farm = getFarmInfo(save)
-generateFarm('spring', farm).show()
