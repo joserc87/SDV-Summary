bc8bffd47ab59761fe6646e5827a288778052ff9
Laukei <slavetothewage@gmail.com>
2016-10-07 23:19:41 +0100

    fixed emailDrone not sending emails (hangover from restructuring of project)

--- sdv/emailDrone.py
+++ sdv/emailDrone.py
@@ -1,9 +1,10 @@
 import json
 from flask import Flask
 from flask_mail import Message
-from sdv import app
+from sdv import app, connect_db, mail
 import uuid
 import time
+sqlesc = app.sqlesc
 
 def email_confirmation(address,user_id,key):
 	title = 'Confirm upload.farm registration'
@@ -24,36 +25,36 @@ def email_passwordreset(address,user_id,key):
 	send_email(address,title,body,html)
 
 def send_email(address, title, body, html):
-	with app.app.app_context():
+	with app.app_context():
 		msg = Message(title,recipients=[address])
 		msg.body = body
 		msg.html = html
-		app.mail.send(msg)
+		mail.send(msg)
 
 def process_email():
 	start_time = time.time()
 	records_handled = 0
-	db = app.connect_db()
+	db = connect_db()
 	cur = db.cursor()
 	while True:
-		cur.execute('UPDATE todo SET currently_processing='+app.app.sqlesc+' WHERE id=(SELECT id FROM todo WHERE task=ANY('+app.app.sqlesc+') AND currently_processing IS NOT TRUE LIMIT 1) RETURNING *',(True,['email_confirmation','old_email_confirmation','email_passwordreset'],))
+		cur.execute('UPDATE todo SET currently_processing='+sqlesc+' WHERE id=(SELECT id FROM todo WHERE task=ANY('+sqlesc+') AND currently_processing IS NOT TRUE LIMIT 1) RETURNING *',(True,['email_confirmation','old_email_confirmation','email_passwordreset'],))
 		tasks = cur.fetchall()
 		db.commit()
 		# print tasks
 		if len(tasks) != 0:
 			for task in tasks:
 				if task[1] in ['email_confirmation','old_email_confirmation']:
-					cur.execute('UPDATE users SET email_conf_token=(CASE WHEN email_conf_token IS NULL THEN '+app.app.sqlesc+' WHEN email_conf_token IS NOT NULL THEN email_conf_token END) WHERE id='+app.app.sqlesc+' RETURNING email, id, email_conf_token',(str(uuid.uuid4()),task[2]))
+					cur.execute('UPDATE users SET email_conf_token=(CASE WHEN email_conf_token IS NULL THEN '+sqlesc+' WHEN email_conf_token IS NOT NULL THEN email_conf_token END) WHERE id='+sqlesc+' RETURNING email, id, email_conf_token',(str(uuid.uuid4()),task[2]))
 					email_data = cur.fetchall()[0]
 					if task[1] == 'email_confirmation':
 						email_confirmation(*email_data)
 					elif task[1] == 'old_email_confirmation':
 						old_email_confirmation(*email_data)
 				elif task[1] in ['email_passwordreset']:
-					cur.execute('UPDATE users SET pw_reset_token='+app.app.sqlesc+' WHERE id='+app.app.sqlesc+' RETURNING email, id, pw_reset_token',(str(uuid.uuid4()),task[2]))
+					cur.execute('UPDATE users SET pw_reset_token='+sqlesc+' WHERE id='+sqlesc+' RETURNING email, id, pw_reset_token',(str(uuid.uuid4()),task[2]))
 					email_data = cur.fetchall()[0]
 					email_passwordreset(*email_data)
-				cur.execute('DELETE FROM todo WHERE id=('+app.app.sqlesc+')',(task[0],))
+				cur.execute('DELETE FROM todo WHERE id=('+sqlesc+')',(task[0],))
 				db.commit()
 				records_handled += 1
 		else:
