121881aeebe89c98333f11ea51a2af03b5e9fabf
Sketchy502 <ashley.john.eldridge@gmail.com>
2017-05-16 23:59:18 +0200

    Tests: Added basic tests for wordfilter.

---
+++ tests/test_wordfilter.py
@@ -0,0 +1,33 @@
+from sdv.parsers.wordfilter.logic import Censor
+
+
+class TestWordfilter():
+    censor = Censor()
+
+    def test_wordfilter_safe_word(self):
+        """
+        Ensure words not on the filter list are unchanged.
+        """
+
+        test_text = 'Hello'
+        result = self.censor.censor(test_text)
+        assert result == test_text
+
+    def test_wordfilter_unsafe_word(self):
+        """
+        Ensure words on the filter list are censored and all characters are from Censor.censorchars.
+        """
+
+        test_text = 'fuck'
+        result = self.censor.censor(test_text)
+        assert result != test_text
+        assert all(char in self.censor.censorchars for char in result)
+
+    def test_wordfilter_unsafe_substring(self):
+        """
+        Ensure substrings are also censored.
+        """
+
+        test_text = 'scunthorpe'
+        result = self.censor.censor(test_text)
+        assert result != test_text
