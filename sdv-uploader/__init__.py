6a4271a9abf4b308a1289e4794fbe0b1a237bd79
Laukei <slavetothewage@gmail.com>
2017-11-05 11:56:09 +0000

    fixed(?) image issue when opening from non-default working directory

--- sdv-uploader/__init__.py
+++ sdv-uploader/__init__.py
@@ -1,9 +1,16 @@
 from windows import launch
 from setup import version
+import os
+import sys
 
 __version__ = version
 
 def main():
+	try:
+		directory = os.path.split(sys.argv[0])[0]
+		os.chdir(directory)
+	except:
+		pass
 	launch()
 
 if __name__ == '__main__':
