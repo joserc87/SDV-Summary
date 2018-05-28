eae60fc9279792fd0000300a69771a0176697d6a
Sketchy502 <ashley.john.eldridge@gmail.com>
2018-05-28 21:13:22 +0200

    Setup profile page javascript

--- sdv/asset_bundles.py
+++ sdv/asset_bundles.py
@@ -2,6 +2,15 @@ from flask_assets import Environment, Bundle
 
 assets = Environment()
 
+# CSS Assets
+
 assets.register('common_css', Bundle(
         './css/style.css',
-        output='./css/common.%(version)s.min.css'))
+        output='./css/common.%(version)s.css'))
+
+# Javascript Assets
+
+assets.register('profile_js', Bundle(
+        './js/profile.js',
+        output='./js/profile.%(version)s.min.js'
+))
