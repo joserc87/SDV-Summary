89ba7705d032e8ddc288935ecc714a5515bc66e6
Jose Ramon Cano <joserc87@gmail.com>
2021-01-30 14:15:02 +0100

    Fix runserver by removing chdir to sdv

--- runserver.py
+++ runserver.py
@@ -1,10 +1,11 @@
 import os
 
-os.chdir(os.path.join(os.path.dirname(__file__), "sdv"))
+# os.chdir(os.path.join(os.path.dirname(__file__), "sdv"))
 
 import sys
 
 sys.path.insert(0, "./")
+sys.path.insert(1, "./sdv/")
 
 # for some reason on Python 3.4 on Linux Mint, using runserver.py crashes on first reload if os.chdir() is used
 # so to avoid this (and break some of the site, but oh well...) remove os.chdir and add sdv to path
